9db852dd68c89e07bebbb83cc73cef97313aade6
Indian1999 <haszab99@gmail.com>
2025-10-30 15:25:28 +0100

    eredmények

--- házi3.py
+++ házi3.py
@@ -5,3 +5,6 @@
 # 2. feladat: Hány tanuló szerzett 5-ös osztályzatot (minimum 91 pont)?
 
 # 3. feladat: A lista hanyadik indexén található a legjobb eredmény?
+
+import random
+eredmények = [random.randint(0, 100) for i in range(25)]
