81dcdce65202722e2aa28ebe8c44bb1a291c522e
Indian1999 <haszab99@gmail.com>
2025-12-20 13:47:10 +0100

    prog vizsga 3

--- prog VIZGSA 2/haz_feladat.py
+++ prog VIZGSA 2/haz_feladat.py
@@ -1,4 +1,12 @@
 # Olvass be egy számot, dönts el hogy páros-e
+a = int(input())
+if a % 2 == 0:
+    pass
+
+b = 3
+c = 5
+if b*c % 10 == 0:
+    print("osztható 10-el")
 
 # Olvass be 2 számot, ha a szorzatuk osztható 10-el, írd ki, hogy a szorzat osztható 10-el
 
